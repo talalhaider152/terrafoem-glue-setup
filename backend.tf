9f5bb0e667c80735b141cc8b00a410ca50198be0
talalhaider152 <talal.haider@ietglobaltech.com>
2025-11-11 02:18:13 +0500

    troubleshooting

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket-1762804809" # Replace with your S3 bucket name
-    key            = "prod&dev/terraform.tfstate" # Path within the S3 bucket
-    region         = "sa-east-1"                     # AWS region of your S3 bucket
-    dynamodb_table = "terraform-locks"    # Replace with your DynamoDB table name for locking
-    encrypt        = true                            # Optional: Enable server-side encryption for the state file
+    key            = "prod&dev/terraform.tfstate"           # Path within the S3 bucket
+    region         = "sa-east-1"                            # AWS region of your S3 bucket
+    dynamodb_table = "terraform-locks"                      # Replace with your DynamoDB table name for locking
+    encrypt        = true                                   # Optional: Enable server-side encryption for the state file
   }
 }
